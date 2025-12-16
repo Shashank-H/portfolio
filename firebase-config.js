dc5fa75b974bc8744a6ea69f8859ebdff4025b2f
Shashank Harikripa <shashankharikripa@gmail.com>
2025-12-16 12:04:12 +0000

    Revert "feat: Add Firebase configuration generation via environment variables, GitHub Pages deployment workflow, and update `.gitignore` rules."
    This reverts commit 2c609fa8d693e111f8f4a8b06cc2d3238dc642a7.

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCAacgNGs_SXhHA5FAI2GONvbpsX9hTiYI",
+  authDomain: "shashank-portfolio-site.firebaseapp.com",
+  projectId: "shashank-portfolio-site",
+  storageBucket: "shashank-portfolio-site.firebasestorage.app",
+  messagingSenderId: "351249282442",
+  appId: "1:351249282442:web:dc0b2ce97467afa40d8473",
+  measurementId: "G-FELVS32KZZ"
+};
